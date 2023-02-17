a4bf5743582dc19dcb1582a194bfe042c7a7ae06
arbertrary <endzeit7@gmail.com>
2023-02-17 13:14:08 +0100

    remove all measurements before loading another scene. Initial try at getting the measurements but isn't completely working right now so currently commented out

--- ws/websocket.js
+++ ws/websocket.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 const express = require('express');
-
+// const PoTree = require('./libs/potree/potree')
 
 // Config
 const Config = {
@@ -32,20 +32,34 @@ wss.on("connection", (ws) => {
 		const msg = JSON.parse(message);
 
 		if (msg.type == "load") {
+
+			// Remove measurements before loading
+			viewer.scene.removeAllMeasurements();
+
 			console.log(msg);
 			Potree.loadProject(viewer, msg.data);
 		}
 		else if (msg.type == "save") {
-			let data = Potree.saveProject(viewer);
-			console.log(data);
-			// let dataString = JSON5.stringify(data, null, "\t");
-			// ws.send(JSON.stringify(dataString));
+			let potreeConfig = Potree.saveProject(viewer);
+			console.log(potreeConfig);
 
-			ws.send(JSON.stringify(data));
+			// let scene = viewer.scene;
+			// let measurements = [...scene.measurements, ...scene.profiles, ...scene.volumes];
+
+			// let geoJson = ""
+			// if (measurements.length > 0) {
+			// 	geoJson = GeoJSONExporter.serialize(measurements);
+			// }
+
+			exportMsg = {
+				potreeConfig: potreeConfig,
+				measurements: "Developer comment: TODO"
+			}
+
+			ws.send(JSON.stringify(exportMsg));
 		} else {
 			console.error("Couldn't get message type");
 		}
-
 	});
 
 
@@ -54,7 +68,115 @@ wss.on("connection", (ws) => {
 	// ws.send("Hi there, I am a WebSocket server");
 });
 
-// start our server
-// server.listen(Config.http_port, () => {
-// 	console.log(`Data stream server started on port ${Config.http_port}`);
-// });
+/**
+ *
+ * @author sigeom sa / http://sigeom.ch
+ * @author Ioda-Net Sàrl / https://www.ioda-net.ch/
+ * @author Markus Schütz / http://potree.org
+ *
+ */
+
+class GeoJSONExporter {
+
+	static measurementToFeatures(measurement) {
+		let coords = measurement.points.map(e => e.position.toArray());
+
+		let features = [];
+
+		if (coords.length === 1) {
+			let feature = {
+				type: 'Feature',
+				geometry: {
+					type: 'Point',
+					coordinates: coords[0]
+				},
+				properties: {
+					name: measurement.name
+				}
+			};
+			features.push(feature);
+		} else if (coords.length > 1 && !measurement.closed) {
+			let object = {
+				'type': 'Feature',
+				'geometry': {
+					'type': 'LineString',
+					'coordinates': coords
+				},
+				'properties': {
+					name: measurement.name
+				}
+			};
+
+			features.push(object);
+		} else if (coords.length > 1 && measurement.closed) {
+			let object = {
+				'type': 'Feature',
+				'geometry': {
+					'type': 'Polygon',
+					'coordinates': [[...coords, coords[0]]]
+				},
+				'properties': {
+					name: measurement.name
+				}
+			};
+			features.push(object);
+		}
+
+		if (measurement.showDistances) {
+			measurement.edgeLabels.forEach((label) => {
+				let labelPoint = {
+					type: 'Feature',
+					geometry: {
+						type: 'Point',
+						coordinates: label.position.toArray()
+					},
+					properties: {
+						distance: label.text
+					}
+				};
+				features.push(labelPoint);
+			});
+		}
+
+		if (measurement.showArea) {
+			let point = measurement.areaLabel.position;
+			let labelArea = {
+				type: 'Feature',
+				geometry: {
+					type: 'Point',
+					coordinates: point.toArray()
+				},
+				properties: {
+					area: measurement.areaLabel.text
+				}
+			};
+			features.push(labelArea);
+		}
+
+		return features;
+	}
+
+	static serialize(measurements) {
+		if (!(measurements instanceof Array)) {
+			measurements = [measurements];
+		}
+
+		// measurements = measurements.filter(m => m instanceof Measure);
+
+		let features = [];
+		for (let measure of measurements) {
+			let f = GeoJSONExporter.measurementToFeatures(measure);
+
+			features = features.concat(f);
+		}
+
+		let geojson = {
+			type: 'FeatureCollection',
+			features: features
+		};
+
+		// return JSON.stringify(geojson, null, '\t');
+		return geojson
+	}
+
+}
