b7a122091b2a80aae32f7ef0e1b733badd3f1e97
arbertrary <endzeit7@gmail.com>
2023-02-21 12:23:47 +0100

    removed server creation. Need only websocket

--- ws/websocket.js
+++ ws/websocket.js
@@ -1,24 +1,15 @@
 const WebSocket = require('ws');
-const express = require('express');
 const PoTree = require('./libs/potree/potree')
 
 // Config
 const Config = {
-	http_port: 8888,
 	socket_port: 3030
 };
-
-// Http server
-const _app = express();
-const server = require('http').Server(_app);
-server.listen(Config.http_port);
 
 
 const wss = new WebSocket.Server({ port: Config.socket_port });
 // Console print
-// Console print
-console.log('[SERVER]: WebSocket on: localhost:' + Config.socket_port); // print websocket ip address
-console.log('[SERVER]: HTTP on: localhost:' + Config.http_port); // print web server ip address
+console.log('[SERVER]: WebSocket on: ws://localhost:' + Config.socket_port); // print websocket ip address
 console.log(wss.address());
 
 
@@ -27,19 +18,16 @@ wss.on("connection", (ws) => {
 	ws.on("message", (message) => {
 		//log the received message and send it back to the client
 		console.log("received: %s", message);
-		// ws.send(`Hello, you sent -> ${message}`);
-
 		const msg = JSON.parse(message);
 
-		if (msg.type == "load") {
-
+		if (msg.type == "Scene") {
 			// Remove measurements before loading
 			viewer.scene.removeAllMeasurements();
 
 			console.log(msg);
 			Potree.loadProject(viewer, msg.data);
 		}
-		else if (msg.type == "save") {
+		else if (msg.type == "GetScene") {
 			let potreeConfig = Potree.saveProject(viewer);
 			console.log(potreeConfig);
 
@@ -52,8 +40,11 @@ wss.on("connection", (ws) => {
 			}
 
 			exportMsg = {
-				potreeConfig: potreeConfig,
-				geoJSONMeasurements: geoJson
+				type: "Scene",
+				data: {
+					potreeConfig: potreeConfig,
+					geoJSONMeasurements: geoJson
+				}
 			}
 			try {
 				ws.send(JSON.stringify(exportMsg));
@@ -66,15 +57,10 @@ wss.on("connection", (ws) => {
 			console.error("Couldn't get message type");
 		}
 	});
-
-
-
-	//send immediatly a feedback to the incoming connection
-	// ws.send("Hi there, I am a WebSocket server");
 });
 
 // /**
-//  *
+//  * see GeoJSONExporter in ../libs/potree/potree.js 
 //  * @author sigeom sa / http://sigeom.ch
 //  * @author Ioda-Net Sàrl / https://www.ioda-net.ch/
 //  * @author Markus Schütz / http://potree.org
@@ -100,6 +86,5 @@ function serializeMeasurements(measurements) {
 		features: features
 	};
 
-	// return JSON.stringify(geojson, null, '\t');
 	return geojson
 }
